5041d246a7ceb56bb9d4137b9e24c6b5198167d9
Anel Danza <aneldanza23@gmail.com>
2024-08-13 16:24:17 -0400

    create tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_200419) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_13_202140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
