8833002fe65a3109f018712d23aeeb444e6e13c6
Anel Danza <aneldanza23@gmail.com>
2024-09-13 14:45:55 -0400

    add TagSerializer

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
     @tags = Tag.all
-    render json: @tags
+    render json: @tags.map { |tag| TagSerializer.new(tag).serializable_hash[:data][:attributes] }
   end
 end
