7e07a96c1e55f8ac7de9ef0877ae5f17bc03bbfd
Anel Danza <aneldanza23@gmail.com>
2024-09-13 14:19:54 -0400

    add description to tag model

---
+++ db/migrate/20240913181736_add_description_to_tags_table.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToTagsTable < ActiveRecord::Migration[7.1]
+  def change
+    add_column :tags, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_13_204740) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_13_181736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_13_204740) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "tags_users", id: false, force: :cascade do |t|
