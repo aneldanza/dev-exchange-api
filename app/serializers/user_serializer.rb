44535bb7768dd600f5c8097b5d77c939d628cd57
Anel Danza <aneldanza23@gmail.com>
2024-09-24 13:55:56 -0400

    allow viewing limited user information if user is not signed in

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :username
+  attributes :id, :username, :email, :created_at, :updated_at
+
+  has_many :tags, serializer: TagSerializer
+
+  attribute :tags do |object|
+    object.tags.map { |tag| TagSerializer.new(tag).serializable_hash[:data][:attributes] }
+  end
 end
