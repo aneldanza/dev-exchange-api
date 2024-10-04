797490b248b5ab0b21c78171f7afe9e0c6952f25
Anel Danza <aneldanza23@gmail.com>
2024-10-04 14:44:51 -0400

    change authorize_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,35 +38,44 @@ class ApplicationController < ActionController::API
 
   def authorize_user
     Rails.logger.debug("*** inside authorize_user method")
-    # Extract the JWT token from the cookies
-    token = cookies.signed[:jwt]
 
-    if token.present?
-      begin
-        # Decode the JWT token
-        decoded_token = JWT.decode(token, Rails.application.credentials.devise_jwt_secret_key!, true, algorithm: "HS256")
-        jti = decoded_token.first["jti"]
-
-        # Find the user based on the jti
-        @current_user = User.find_by(jti: jti)
-        Rails.logger.debug("*** curent user in authorize_user is: #{@current_user.jti}")
-
-        # Handle case where user is not found
-        if @current_user.nil?
-          render json: { error: "Unauthorized" }, status: :unauthorized
-        end
-      rescue JWT::DecodeError => e
-        Rails.logger.error "JWT decode error: #{e.message}"
-        render json: { error: "Unauthorized" }, status: :unauthorized
-      end
-    else
-      render json: { error: "No token provided" }, status: :unauthorized
+    if !logged_in?
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
+
+    # Extract the JWT token from the cookies
+    # token = cookies.signed[:jwt]
+
+    # if token.present?
+    #   begin
+    #     # Decode the JWT token
+    #     decoded_token = JWT.decode(token, Rails.application.credentials.devise_jwt_secret_key!, true, algorithm: "HS256")
+    #     jti = decoded_token.first["jti"]
+
+    #     # Find the user based on the jti
+    #     @current_user = User.find_by(jti: jti)
+    #     Rails.logger.debug("*** curent user in authorize_user is: #{@current_user.jti}")
+
+    #     # Handle case where user is not found
+    #     if @current_user.nil?
+    #       render json: { error: "Unauthorized" }, status: :unauthorized
+    #     end
+    #   rescue JWT::DecodeError => e
+    #     Rails.logger.error "JWT decode error: #{e.message}"
+    #     render json: { error: "Unauthorized" }, status: :unauthorized
+    #   end
+    # else
+    #   render json: { error: "No token provided" }, status: :unauthorized
+    # end
   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username])
+  end
+
+  def logged_in?
+    !!@current_user
   end
 
   private
