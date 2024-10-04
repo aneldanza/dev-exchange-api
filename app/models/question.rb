2daf3d7c94c21dde4cb7f569831ca546cdd2e769
Anel Danza <aneldanza23@gmail.com>
2024-10-04 14:27:17 -0400

    add custom validation to ensure that question has at least one tag

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,11 @@ class Question < ApplicationRecord
   has_and_belongs_to_many :tags
 
   validates_presence_of :title, :body
+  validate :must_have_tags
+
+  private
+
+  def must_have_tags
+    errors.add(:base, "must have at least one tag") if tags.empty?
+  end
 end
