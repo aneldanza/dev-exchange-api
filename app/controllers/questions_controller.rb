1bd09ff6cf40507cd12dc4f13810ff0dc44add91
Anel Danza <aneldanza23@gmail.com>
2024-10-28 17:09:54 -0400

    update user and question tags if queestion was created or updated successfully

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,11 +13,15 @@ class QuestionsController < ApplicationController
 
   def create
     tagsData = params[:question].delete(:tags)
-    tags = Tag.where(id: tagsData.map { |tag| tag[:id] })
+    tags = tagsData.map do |tag|
+      findOrCreateTag(tag)
+    end
     @question = Question.new(question_params)
     @question.tags << tags
 
     if @question.save
+      user = User.find(@question.user_id)
+      updateRecordTags(user, tags)
       render json: QuestionSerializer.new(@question).serializable_hash[:data][:attributes], status: 201
     else
       render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
@@ -26,7 +30,15 @@ class QuestionsController < ApplicationController
 
   def update
     @question = Question.includes(:tags).find(params[:id])
+    tagsData = params[:question].delete(:tags)
+    tags = tagsData.map do |tag|
+      findOrCreateTag(tag)
+    end
+
     if @question.update(question_params)
+      user = User.find(@question.user_id)
+      updateRecordTags(user, tags)
+      updateRecordTags(@question, tags)
       render json: QuestionSerializer.new(@question).serializable_hash[:data][:attributes], status: 200
     else
       render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
@@ -42,6 +54,20 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:title, :body, :user_id, :tags)
+    params.require(:question).permit(:title, :body, :user_id, :tags, :tag_ids)
+  end
+
+  def findOrCreateTag(tag_data)
+    if tag_data[:id] == 0
+      Tag.create(name: tag_data[:name])
+    else
+      Tag.find(tag_data[:id])
+    end
+  end
+
+  def updateRecordTags(record, tags)
+    tags.each do |tag|
+      record.tags << tag unless record.tags.include?(tag)
+    end
   end
 end
