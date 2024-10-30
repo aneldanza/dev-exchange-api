0522f55b9f905c01e7a49aa184e920377097ecea
Anel Danza <aneldanza23@gmail.com>
2024-10-30 18:12:02 -0400

    update questions array

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -3,4 +3,8 @@ class TagSerializer
   include JSONAPI::Serializer
 
   attributes :id, :name, :description, :created_at
+
+  attribute :questions do |object|
+    object.questions.map { |question| QuestionSerializer.new(question).serializable_hash[:data][:attributes] }
+  end
 end
