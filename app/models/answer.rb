a6b496a012f2a4a5467ecfcf8c865752bdc37eac
Anel Danza <aneldanza23@gmail.com>
2024-11-05 16:32:29 -0500

    use rich_text for body of answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
+  has_rich_text :body
   belongs_to :user
   belongs_to :question
 
--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,7 +1,11 @@
 class AnswerSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :body, :created_at, :updated_at, :question_id
+  attributes :id, :created_at, :updated_at, :question_id
+
+  attribute :body do |object|
+    object.body.to_trix_html
+  end
 
   attribute :user do |object|
     {
