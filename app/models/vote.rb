50aa0829f481e4d51478e5473f6d64f0c6a0695f
Anel Danza <aneldanza23@gmail.com>
2024-11-12 15:05:13 -0500

    add validations to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
+
+  validates :user_id, uniqueness: { scope: %i[votable_type votable_id] }
+  validates :value, inclusion: { in: [-1, 1] }
 end
