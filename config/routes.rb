ef5777c141f2ed944c0ddc34aa887161a371a0c2
Anel Danza <aneldanza23@gmail.com>
2024-11-12 15:20:32 -0500

    create vote post route

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :questions, only: %i[index show create update destroy]
   resources :answers, only: %i[index show create update destroy]
   resources :comments, only: %i[show create update destroy]
+  resources :votes, only: %i[create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
