032caf34cace699151da8808c1dfc65e45e98ff8
Anel Danza <aneldanza23@gmail.com>
2024-11-12 15:20:18 -0500

    forbid user to vote on their own post

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,10 +3,16 @@ class VotesController < ApplicationController
 
   def create
     @votable = params[:votable_type].constantize.find(params[:votable_id])
+
+    if is_owner?
+      render json: { errors: ["You can't vote for your own content"] }, status: :forbidden
+      return
+    end
+
     @vote = @votable.votes.new(vote_params)
     @vote.user = current_user
     if @vote.save
-      render json: { votable_id: @votable.id, votable_type: @votable.class.to_s, value: @vote.value }
+      render json: { votable_id: @votable.id, votable_type: @votable.class.to_s, value: @vote.value }, status: :ok
     else
       render json: { errors: @vote.errors.full_messages }, status: :unprocessable_entity
     end
@@ -16,5 +22,9 @@ class VotesController < ApplicationController
 
   def vote_params
     params.require(:vote).permit(:value, :votable_id, :votable_type)
+  end
+
+  def is_owner?
+    @votable.user == current_user
   end
 end
