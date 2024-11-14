1606710f3198f200a29c23b90eaf994574bd509a
Anel Danza <aneldanza23@gmail.com>
2024-11-14 15:15:31 -0500

    use pg_search to create search_by_name method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,20 @@
 class Tag < ApplicationRecord
+  include PgSearch::Model
+
   has_and_belongs_to_many :users
   has_and_belongs_to_many :questions
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   before_validation :downcase_name
+
+  pg_search_scope :search_by_name,
+                  against: :name,
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      normalization: 2,
+                    },
+                  }
 
   private
 
