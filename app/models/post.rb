9b4d1445eacf09821e79ae66eb3890b381ea3209
Anel Danza <aneldanza23@gmail.com>
2024-12-10 19:05:21 -0500

    update Post, Question and Answer models for Single Table Inheritance flow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,33 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :question, optional: true
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :votes, as: :votable, dependent: :destroy
+  has_and_belongs_to_many :tags
+  has_rich_text :body
+
+  validates :body, presence: true
+  validates :title, presence: true, if: :question?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_body,
+                  against: :title,
+                  associated_against: {
+                    rich_text_body: [:body],
+                  },
+                  using: {
+                    tsearch: { prefix: true },
+                  }
+
+  def question?
+    type == "Question"
+  end
+
+  def answer?
+    type == "Answer"
+  end
+
+  def score
+    votes.sum(:value)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,8 @@
-class Question < ApplicationRecord
+class Question < Post
   include PgSearch::Model
 
-  has_rich_text :body
-  belongs_to :user
-  has_and_belongs_to_many :tags
   has_many :answers, dependent: :destroy
-  has_many :comments, as: :commentable, dependent: :destroy
-  has_many :votes, as: :votable, dependent: :destroy
 
-  validates_presence_of :title, :body
   validates_uniqueness_of :title
   validate :must_have_tags
 
@@ -21,18 +15,14 @@ class Question < ApplicationRecord
                     tsearch: { prefix: true },
                   }
 
-  pg_search_scope :search_by_title_and_body,
-                  against: %i[title],
-                  associated_against: {
-                    rich_text_body: :body,
-                  },
-                  using: {
-                    tsearch: { prefix: true },
-                  }
-
-  def score
-    votes.sum(:value)
-  end
+  # pg_search_scope :search_by_title_and_body,
+  #                 against: %i[title],
+  #                 associated_against: {
+  #                   rich_text_body: :body,
+  #                 },
+  #                 using: {
+  #                   tsearch: { prefix: true },
+  #                 }
 
   private
 
