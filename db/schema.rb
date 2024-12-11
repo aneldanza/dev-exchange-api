e2efc4df53260874f1dc19853cb4799bbc63c9c9
Anel Danza <aneldanza23@gmail.com>
2024-12-10 19:10:30 -0500

    create join table for posts and tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_10_232034) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_11_000928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -83,6 +83,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_10_232034) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
   end
 
   create_table "questions", force: :cascade do |t|
