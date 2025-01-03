429bbc70cd21c7d41250f8793d06be28aa8f9c49
Anel Danza <aneldanza23@gmail.com>
2025-01-02 22:41:09 -0500

    check if curent user is also the answer author before processing update and destroy actions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authorize_user, only: [:create, :update, :destroy]
   before_action :set_answer, only: [:update, :destroy]
 
   # GET /answers
@@ -27,6 +28,10 @@ class AnswersController < ApplicationController
 
   # PATCH/PUT /answers/1
   def update
+    if !author?(@answer.user_id)
+      render json: { error: "Unauthorized" }, status: :unauthorized
+      return
+    end
     if @answer.update(answer_params)
       render json: AnswerSerializer.new(@answer).serializable_hash[:data][:attributes], status: :ok
     else
@@ -36,6 +41,10 @@ class AnswersController < ApplicationController
 
   # DELETE /answers/1
   def destroy
+    if !author?(@answer.user_id)
+      render json: { error: "Unauthorized" }, status: :unauthorized
+      return
+    end
     @answer.destroy
   end
 
