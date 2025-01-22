9a0bcf899e3d4a6aad7ea0ac2eb8c607b722c1eb
Anel Danza <aneldanza23@gmail.com>
2025-01-22 16:46:13 -0500

    update allowed origin list with currently deployed dev-exchange frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:5173", "https://dev-exchange-9c70c7f656d7.herokuapp.com"] # later change to the domain of the frontend app
+    origins ["http://localhost:5173", "https://dev-exchange-dk5n.vercel.app"] # later change to the domain of the frontend app
 
     resource "*",
       headers: :any,
